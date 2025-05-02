1a1b9e9b0082370f8f0b57563ed1cf4187d0c1a9
sanjanays4 <sanjana.ys284@gmail.com>
2025-05-02 13:02:41 +0530

    Update student.java

--- student.java
+++ student.java
@@ -1,6 +1,6 @@
 class student {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello!!!");
         System.out.println("World");        
     }
 }
